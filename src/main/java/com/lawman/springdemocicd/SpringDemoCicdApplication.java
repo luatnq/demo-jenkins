79e83901ea0be7d02284375ebb9a1a68ac71d5e1
luatnq <nguyenquocluat1203@gmail.com>
2023-08-21 09:38:41 +0700

    first commit

---
+++ src/main/java/com/lawman/springdemocicd/SpringDemoCicdApplication.java
@@ -0,0 +1,13 @@
+package com.lawman.springdemocicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoCicdApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringDemoCicdApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/lawman/springdemocicd/SpringDemoCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawman.springdemocicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoCicdApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
